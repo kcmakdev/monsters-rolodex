eff4c38102fc0405a90a02d739224c72c22a9b9b
makkacheong <gaku113@hotmail.com>
2019-11-18 15:34:40 +0800

    Updates

---
+++ precache-manifest.395ccfeb596572c5b5c70d2034e29221.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fee547aa74699bb4080d7e75d7ec43",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4a73d68381ca76974101",
+    "url": "/monsters-rolodex/static/css/main.8ce8568a.chunk.css"
+  },
+  {
+    "revision": "8256d7d0304953484fc5",
+    "url": "/monsters-rolodex/static/js/2.f5605220.chunk.js"
+  },
+  {
+    "revision": "4a73d68381ca76974101",
+    "url": "/monsters-rolodex/static/js/main.ce854120.chunk.js"
+  },
+  {
+    "revision": "1b1f57fd6c4b40aa49d9",
+    "url": "/monsters-rolodex/static/js/runtime-main.10abe5f5.js"
+  }
+]);
